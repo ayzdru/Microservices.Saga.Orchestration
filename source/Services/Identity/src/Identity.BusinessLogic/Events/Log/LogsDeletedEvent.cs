d975eb4653e7aa3e49e8007967e2ea323c903a37
Ayaz <ayzdru@gmail.com>
2026-02-27 01:05:05 +0300

    Eksik Log klasörleri eklendi.

---
+++ source/Services/Identity/src/Identity.BusinessLogic/Events/Log/LogsDeletedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Identity.AuditLogging.Events;
+
+namespace Identity.BusinessLogic.Events.Log;
+
+public class LogsDeletedEvent : AuditEvent
+{
+    public LogsDeletedEvent(DateTime deleteOlderThan)
+    {
+        DeleteOlderThan = deleteOlderThan;
+    }
+
+    public DateTime DeleteOlderThan { get; set; }
+}
---
+++ source/Services/Identity/src/Identity.BusinessLogic/Events/Log/LogsRequestedEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Identity.AuditLogging.Events;
+
+namespace Identity.BusinessLogic.Events.Log;
+
+public class LogsRequestedEvent : AuditEvent
+{
+}
